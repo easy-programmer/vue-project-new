36748b3f8b1e4ce01d1bdee0117e949f97073b8b
ohardcore <ssoguy3@gmail.com>
2018-10-07 19:45:34 +0900

    vue table commit

--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueGoodTablePlugin from 'vue-good-table'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vue-good-table/dist/vue-good-table.css'
 
 Vue.config.productionTip = false
 
 Vue.use(VueGoodTablePlugin)
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
