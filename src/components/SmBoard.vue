3080924c0ee84934012b6d4758e0c28263c740e7
tjdah13 <tjdah0516@naver.com>
2018-10-08 14:00:57 +0900

    개인계정 pork project Link

--- src/components/SmBoard.vue
+++ src/components/SmBoard.vue
@@ -1,5 +1,11 @@
 <template>
-  <div> Dash board Test </div>
+  <div> 
+    Dash board Test <br/>
+    <span>
+      >> 대시보드/Admin 화면 pork ---->
+      <a href='https://github.com/tjdah13/vuestic-admin' >https://github.com/tjdah13/vuestic-admin</a>
+    </span>
+  </div>
 </template>
 
 <script>
