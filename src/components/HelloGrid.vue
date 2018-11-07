75cd57a2a9cf0771f209e514b79ad4e46ad2c482
ohardcore <ssoguy3@gmail.com>
2018-11-07 18:56:05 +0900

    bootstrap json-server 연동

--- src/components/HelloGrid.vue
+++ src/components/HelloGrid.vue
@@ -1,45 +1,23 @@
 <template>
-  <b-table striped hover :items="items" :fields="fields"></b-table>
+  <b-table striped hover :items="items"></b-table>
 </template>
 <!-- npm i bootstrap-vue --save -->
 <!-- npm install jquery@3.3.1 --save -->
 <!-- npm install --dev url-loader@1.1.1 --save -->
 <script>
-    export default {
-      data () {
-        return {
-          // Note 'isActive' is left out and will not appear in the rendered table
-          fields: {
-            last_name: {
-              label: 'Person last name',
-              sortable: true
-            },
-            first_name: {
-              label: 'Person first name',
-              sortable: false
-            },
-            foo: {
-              // This key overrides `foo`!
-              key: 'age',
-              label: 'Person age',
-              sortable: true
-            },
-            city: {
-              key: 'address.city'
-            },
-            'address.country': {
-              label: 'Country'
-            }
-          },
-          items: [
-            { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald', address: { country: 'USA', city: 'New York' } },
-            { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw', address: { country: 'Canada', city: 'Toronto' }},
-            { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson', address: { country: 'Australia', city: 'Sydney' } },
-            { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney', address: { country: 'England', city: 'London' } }
-          ]
-        }
+  const items = [  ]
+  
+  export default {
+    data () {
+      return {
+        items: []
       }
-    }
+    }, mounted() {
+          this.axios.get('http://localhost:3000/data').then(res => {
+            this.items = res.data
+          })
+      }
+  }
   </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App'
 import router from './router'
 import VueGoodTablePlugin from 'vue-good-table'
 import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -12,6 +14,7 @@ import 'vue-good-table/dist/vue-good-table.css'
 
 Vue.config.productionTip = false
 
+Vue.use(VueAxios, axios)
 Vue.use(VueGoodTablePlugin)
 Vue.use(BootstrapVue)
 
